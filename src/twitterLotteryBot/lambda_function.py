bdcdcfc02c7fdfab35862e6cafa4ba264818093a
runau <runa@engineergirl.work>
2020-03-07 22:00:12 +0900

    リファクタリング

--- src/twitterLotteryBot/lambda_function.py
+++ src/twitterLotteryBot/lambda_function.py
@@ -26,7 +26,7 @@ def getParam(key):
     paramTable = dynamoDB.Table("twitterLotPram")
     item = paramTable.get_item(Key={"key": key})
     if "Item" in item:
-        return item["Item"]
+        return item["Item"]["data"]
     else:
         return []
 
@@ -44,10 +44,11 @@ def getItem(key):
     if "Item" in item:
         return item["Item"]
     else:
-        return None
+        return key
 
 
 def putItem(data):
+    print(f"putItem:{data}")
     dynamoDB = boto3.resource("dynamodb")
     table = dynamoDB.Table("twitterLotterBot")
     table.put_item(Item=data)
@@ -60,22 +61,21 @@ def main():
 
     # botのツイートを取得
     doneList = requests.get(
-        f'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={SCREEN_NAME}&count=10',
+        f'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={SCREEN_NAME}&count=20',
         auth=getOauth()
     ).json()
     if "errors" in doneList:
         print(doneList)
         return
-    doneList = getParam("doneList") + \
-        list(map(lambda x: x["in_reply_to_status_id"], doneList))
-    doneList = list(set(doneList))
+    doneList = list(map(lambda x: x["in_reply_to_status_id_str"], doneList))
+    doneList = list(set(getParam("doneList") + doneList))
     putParam("doneList", doneList)
     print(doneList)
 
     # 指定タグのツイートを取得
     search = '%23抽選bot'
     response = requests.get(
-        f'https://api.twitter.com/1.1/search/tweets.json?q={search}&count=50&lang=ja&result_type=mixed&tweet_mode=extended',
+        f'https://api.twitter.com/1.1/search/tweets.json?q={search}&count=20&lang=ja&result_type=mixed&tweet_mode=extended',
         auth=getOauth()
     ).json()
 
@@ -86,7 +86,7 @@ def main():
     statusList = response["statuses"]
 
     tweetList = getParam("tweetList") + \
-        list(map(lambda x: x["statuses"]['id'], response))
+        list(map(lambda x: x['id_str'], statusList))
     tweetList = list(set(tweetList))
     putParam("tweetList", tweetList)
 
@@ -95,14 +95,14 @@ def main():
         if "retweeted_status" in status:
             print("RT skip")
             continue
-        if status["in_reply_to_status_id"]:
+        if status["in_reply_to_status_id_str"]:
             print("rep skip")
             continue
 
-        data = {"statusId": status['id']}
+        tweet_id = status['id_str']
+        data = {"statusId": tweet_id}
         data = getItem(data)
 
-        tweet_id = status['id']
         main_text = status['full_text']
 
         if "…" in main_text:
@@ -113,32 +113,36 @@ def main():
             main_text = tmp['full_text']
 
         data["text"] = main_text
-        data["user_id"] = status['user']['id']
+        data["user_id"] = status['user']['id_str']
         data["user_screen_name"] = status['user']['screen_name']
 
         lot_pro = re.findall(r'(\[.+?\]\(\d+)', main_text)
         lot_pro = list(map(lambda x: {"name": re.findall(
             r'\[(.+)\]', x)[0], "pro": int(re.findall(r'(\d+)', x)[0])}, lot_pro))
-        data["lot_pro"] = lot_pro
 
         lot_message = ""
         for p in lot_pro:
             lot_message += f'{p["name"]}:{p["pro"]}%,'
-        data["lot_message"] = lot_message
 
+        #未リプライの募集ツイートのみ処理する
         if tweet_id not in doneList:
             print(f"{tweet_id} first reply exec")
 
+            #リプライメッセージ作成
+            if "lot_pro" not in data:
+                if len(lot_pro) == 0:
+                    message = f'#抽選bot のご利用ありがとうございます！\n申し訳ありませんが、確立を読み取れませんでした。\n正しい使い方などは↓のリンクをご覧ください。\nhttps://encr.jp/blog/posts/20200306_morning/\n\n※このツイートはbotからの自動送信です'
+                else:
+                    data["lot_pro"] = lot_pro
+                    data["lot_message"] = lot_message
+                    message = f'#抽選bot のご利用ありがとうございます！\n{lot_message[:-1]}で抽選いたします。\nhttps://encr.jp/blog/posts/20200306_morning/\n\n※このツイートはbotからの自動送信です'
+                data["main_reply_message"] = message
+
             # リプライする
-            if len(lot_pro) == 0:
-                message = f'#抽選bot のご利用ありがとうございます！\n申し訳ありませんが、確立を読み取れませんでした。\n正しい使い方などは↓のリンクをご覧ください。\nhttps://encr.jp/blog/posts/20200306_morning/\n\n※このツイートはbotからの自動送信です'
-            else:
-                message = f'#抽選bot のご利用ありがとうございます！\n{lot_message[:-1]}で抽選いたします。\nhttps://encr.jp/blog/posts/20200306_morning/\n\n※このツイートはbotからの自動送信です'
-            data["main_reply_message"] = message
             in_reply_to_status_id = tweet_id
             replyResponse = requests.post(
                 f'https://api.twitter.com/1.1/statuses/update.json',
-                data={"status": message, "in_reply_to_status_id": in_reply_to_status_id,
+                data={"status": message, "in_reply_to_status_id": int(in_reply_to_status_id),
                       "auto_populate_reply_metadata": True},
                 auth=getOauth()
             ).json()
@@ -146,9 +150,11 @@ def main():
             if "errors" in replyResponse:
                 print(replyResponse)
                 return
+            
+            putItem(data)
+
 
         if len(lot_pro) == 0:
-            putItem(data)
             continue
 
         sleepTime = random.random() * 20
@@ -157,7 +163,7 @@ def main():
         # 指定タグのツイート者宛のリプライを取得
         search = f"%40{status['user']['screen_name']}"
         repResponse = requests.get(
-            f'https://api.twitter.com/1.1/search/tweets.json?q={search}&count=10&lang=ja&result_type=mixed',
+            f'https://api.twitter.com/1.1/search/tweets.json?q={search}&count=30&lang=ja&result_type=mixed',
             auth=getOauth()
         ).json()
 
@@ -172,16 +178,16 @@ def main():
                 print("RT skip")
                 continue
 
-            if rep_status['in_reply_to_status_id'] != tweet_id:
+            if rep_status['in_reply_to_status_id_str'] != tweet_id:
                 continue
 
-            if rep_status['id'] in doneList or rep_status['user']['screen_name'] == SCREEN_NAME:
+            if rep_status['id_str'] in doneList or rep_status['user']['screen_name'] == SCREEN_NAME:
                 print(f"{rep_status['user']['name']} reply done")
                 continue
 
             print(f"text:{rep_status['text']}")
-            print(f"tweet_id:{rep_status['id']}")
-            print(f"user_id:{rep_status['user']['id']}")
+            print(f"tweet_id:{rep_status['id_str']}")
+            print(f"user_id:{rep_status['user']['id_str']}")
             print(f"user_screen_name:{rep_status['user']['screen_name']}")
             print(f"user_name:{rep_status['user']['name']}")
 
@@ -195,13 +201,14 @@ def main():
                     lot = p["name"]
                     break
 
-            # リプライする
+            # リプライメッセージ作成
             if lot is not None:
                 message = f'{rep_status["user"]["name"]}さん、リプありがとうございます！\n「{lot}」が当選しました。\n\n※このツイートはbotからの自動送信です #抽選bot'
             else:
                 message = f'{rep_status["user"]["name"]}さん、リプありがとうございます！\n残念ながら、今回は落選してしまいました…\n\n※このツイートはbotからの自動送信です #抽選bot'
 
-            in_reply_to_status_id = rep_status['id']
+            # リプライする
+            in_reply_to_status_id = rep_status['id_str']
             print(f"message:{message} exec")
             print(f"in_reply_to_status_id:{in_reply_to_status_id} exec")
             replyResponse = requests.post(
@@ -211,7 +218,6 @@ def main():
                 auth=getOauth()
             ).json()
             print(replyResponse)
-        putItem(data)
 
 
 def lambda_handler(event, context):
