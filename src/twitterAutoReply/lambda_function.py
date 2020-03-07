7e3b6764089fb3cfc05e18ad45da47a0514e2d81
runau <runa@engineergirl.work>
2020-03-07 20:59:02 +0900

    dynamoにデータ格納する

--- src/twitterAutoReply/lambda_function.py
+++ src/twitterAutoReply/lambda_function.py
@@ -15,137 +15,170 @@ OAUTH3 = os.environ['OAUTH3']
 OAUTH4 = os.environ['OAUTH4']
 SCREEN_NAME = os.environ['SCREEN_NAME']
 
+
 def getOauth():
     return OAuth1(OAUTH1, OAUTH2, OAUTH3, OAUTH4)
 
+
+def getParam(key):
+    dynamoDB = boto3.resource("dynamodb")
+    paramTable = dynamoDB.Table("twitterLotPram")
+    item = paramTable.get_item(Key={"key": key})
+    if "Item" in item:
+        return item["Item"]
+    else:
+        return []
+
+
+def putParam(key, data):
+    dynamoDB = boto3.resource("dynamodb")
+    paramTable = dynamoDB.Table("twitterLotPram")
+    paramTable.put_item(Item={"key": key, "data": data})
+
+
+def getItem(key):
+    dynamoDB = boto3.resource("dynamodb")
+    table = dynamoDB.Table("twitterLotterBot")
+    item = table.get_item(Key={"key": key})
+    if "Item" in item:
+        return item["Item"]
+    else:
+        return None
+
+
+def putItem(data):
+    dynamoDB = boto3.resource("dynamodb")
+    table = dynamoDB.Table("twitterLotterBot")
+    table.put_item(Item=data)
+
+
 def main():
 
-    ngList=["融資",'R18','R-18','例のアレ','アヘ顔','亀頭','へんたい','ヘンタイ','変態','パンチラ',
-            'アッー','アナル','アヘ顔','イマラ','淫','エッチ','おっぱい','オッパイ','オナシャス','ガチムチ',
-            '姦','元祖羞恥心','亀頭','糞','クルルァ','グロ','ゲイ',
-            '18禁','春画','処女','ショタ','パイパン','フェラ','ふたなり','ペニス','へんたい','ヘンタイ','変態','ホモ','まんこ','マンコ','野獣','幼女','レイプ','レズ','セックス','せっくす',
-            'アダルト','セックス','バイブレーター','マスターベーション','オナニー','スケベ','羞恥','セクロス',
-            'エッチ','SEX','風俗','童貞','ペニス','巨乳','ロリ','触手','羞恥','ノーブラ',
-            '大麻','麻薬',
-            '基地外','糞','死ね','殺す',"オフパコ"]
+    ngList = getParam("ngList")
 
-    #自分宛のリプライ最新から10件を全て取得
+    # 自分宛のリプライ最新から10件を全て取得
     response = requests.get(
         f'https://api.twitter.com/1.1/statuses/mentions_timeline.json?count=20',
         auth=getOauth()
     ).json()
 
-    #その中から、指定した元ツイートに対してのリプライのみに絞り込む
-    in_reply_to_status_id=1231358267516809216 #1231087256556859392
-    response = list(filter(lambda x:x["in_reply_to_status_id"] == in_reply_to_status_id,response))
+    # その中から、指定した元ツイートに対してのリプライのみに絞り込む
+    in_reply_to_status_id = 1231358267516809216  # 1231087256556859392
+    response = list(
+        filter(lambda x: x["in_reply_to_status_id"] == in_reply_to_status_id, response))
 
-    #未リプライの物のみに絞り込む
+    # 未リプライの物のみに絞り込む
     doneList = requests.get(
         f'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={SCREEN_NAME}&count=100',
         auth=getOauth()
     ).json()
-    doneList = list(map(lambda x:x["in_reply_to_status_id"],doneList))
+    doneList = list(map(lambda x: x["in_reply_to_status_id"], doneList))
     print(doneList)
-    
-    response = list(filter(lambda x:x["id"] not in doneList,response))
+
+    response = list(filter(lambda x: x["id"] not in doneList, response))
     print(response)
 
-    #扱いやすいように成型
-    replyList = list(map(lambda x:{"id":x["user"]["id"],"name":x["user"]["name"],"profile":x["user"]["description"],"replyId":x["id"],"ng":False},response))
+    # 扱いやすいように成型
+    replyList = list(map(lambda x: {"id": x["user"]["id"], "name": x["user"]["name"],
+                                    "profile": x["user"]["description"], "replyId": x["id"], "ng": False}, response))
     print(replyList)
-    
-    #名前の切り抜き
-    for idx,r in enumerate(replyList):
+
+    # 名前の切り抜き
+    for idx, r in enumerate(replyList):
         name = replyList[idx]["name"]
         name = name.split("@")[0]
         name = name.split("｜")[0]
-        
-        #空になってしまったら、元々の名前を復活
+
+        # 空になってしまったら、元々の名前を復活
         if len(name) == 0:
             name = replyList[idx]["name"]
 
-        #切り抜いた結果of切り抜けなかった結果15文字以内なら文字数で切り取り
+        # 切り抜いた結果of切り抜けなかった結果15文字以内なら文字数で切り取り
         if len(name) <= 15:
             replyList[idx]["name"] = name
         else:
             replyList[idx]["name"] = f"{name[0:15]}…"
 
-    #最新のツイートを10件取得する
+    # 最新のツイートを10件取得する
     option = 'count=10&exclude_replies=true&include_rts=false'
-    for idx,r in enumerate(replyList):
+    for idx, r in enumerate(replyList):
         user_timeline = requests.get(
             f'https://api.twitter.com/1.1/statuses/user_timeline.json?id={r["id"]}&{option}',
             auth=getOauth()
         ).json()
-        replyList[idx]["tweetList"] = list(map(lambda x:x["text"],user_timeline))
-        replyList[idx]["tweetIdList"] = list(map(lambda x:x["id"],user_timeline))
+        replyList[idx]["tweetList"] = list(
+            map(lambda x: x["text"], user_timeline))
+        replyList[idx]["tweetIdList"] = list(
+            map(lambda x: x["id"], user_timeline))
     print(replyList)
 
-    #NGワードチェック
+    # NGワードチェック
     print("NGワードチェック")
-    for idx,reply in enumerate(replyList):
-        #プロフィールをチェック
-        if len(list(filter(lambda ng:ng in reply["profile"],ngList))):
+    for idx, reply in enumerate(replyList):
+        # プロフィールをチェック
+        if len(list(filter(lambda ng: ng in reply["profile"], ngList))):
             print(f"{reply['name']}:ng")
-            print(list(filter(lambda ng:ng in reply["profile"],ngList)))
+            print(list(filter(lambda ng: ng in reply["profile"], ngList)))
             replyList[idx]["ng"] = True
             replyList[idx]["ngReason"] = "プロフィールにNGワードが含まれている"
-        elif len(list(filter(lambda x:len(list(filter(lambda ng:ng in x,ngList))),reply["tweetList"]))):
+        elif len(list(filter(lambda x: len(list(filter(lambda ng: ng in x, ngList))), reply["tweetList"]))):
             print(f"{reply['name']}:ng")
-            print(list(filter(lambda x:len(list(filter(lambda ng:ng in x,ngList))),reply["tweetList"])))
+            print(list(filter(lambda x: len(
+                list(filter(lambda ng: ng in x, ngList))), reply["tweetList"])))
             replyList[idx]["ng"] = True
             replyList[idx]["ngReason"] = "ツイートにNGワードが含まれている"
         else:
             print(f"{reply['name']}:ok")
 
-    #リンク数チェック
+    # リンク数チェック
     print("リンク数チェック")
-    for idx,reply in enumerate(replyList):
-        #ツイートをチェック
-        if len(list(filter(lambda x:x.count("http")>=2,reply["tweetList"]))):
+    for idx, reply in enumerate(replyList):
+        # ツイートをチェック
+        if len(list(filter(lambda x: x.count("http") >= 2, reply["tweetList"]))):
             print(f"{reply['name']}:ng")
             replyList[idx]["ng"] = True
             replyList[idx]["ngReason"] = "ツイートに過剰なリンクがある"
         else:
             print(f"{reply['name']}:ok")
 
-    #重複リンク数チェック
+    # 重複リンク数チェック
     print("重複リンク数チェック")
     pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
-    for idx,reply in enumerate(replyList):
-        #urlを抽出 ※既に1ツイート内に複数urlある場合は除外しているので、ここに来た時には、必ず1ツイート0～1url
-        #まずurlを含むものだけに絞る
-        urlTweetList =list(filter(lambda x:x.count("http")==1,reply["tweetList"]))
+    for idx, reply in enumerate(replyList):
+        # urlを抽出 ※既に1ツイート内に複数urlある場合は除外しているので、ここに来た時には、必ず1ツイート0～1url
+        # まずurlを含むものだけに絞る
+        urlTweetList = list(
+            filter(lambda x: x.count("http") == 1, reply["tweetList"]))
         print(f"urlTweetList:{urlTweetList}")
-        #urlを抽出
-        urlList = list(map(lambda x:re.findall(pattern, x)[0],urlTweetList))
+        # urlを抽出
+        urlList = list(map(lambda x: re.findall(pattern, x)[0], urlTweetList))
         print(f"urlList:{urlList}")
-        #urlListのそれぞれのurlの出現回数をカウントしてくれる ※要import collections
+        # urlListのそれぞれのurlの出現回数をカウントしてくれる ※要import collections
         counter = collections.Counter(urlList)
         print(f"counter:{counter}")
 
-        if len(list(filter(lambda x:x >= 3,counter.values()))):
+        if len(list(filter(lambda x: x >= 3, counter.values()))):
             print(f"{reply['name']}:ng")
             replyList[idx]["ng"] = True
             replyList[idx]["ngReason"] = "ツイートに過剰な重複リンクがある"
         else:
             print(f"{reply['name']}:ok")
 
-    warnList = ["RT企画","固定ツイート","固ツイ","リツイート"]
-    #RT企画数チェック
+    warnList = ["RT企画", "固定ツイート", "固ツイ", "リツイート"]
+    # RT企画数チェック
     print("RT企画数チェック")
-    for idx,reply in enumerate(replyList):
-        if len(list(filter(lambda x:len(list(filter(lambda warn:warn in x,warnList))),reply["tweetList"]))) >= 3:
+    for idx, reply in enumerate(replyList):
+        if len(list(filter(lambda x: len(list(filter(lambda warn: warn in x, warnList))), reply["tweetList"]))) >= 3:
             print(f"{reply['name']}:ng")
             replyList[idx]["ng"] = True
             replyList[idx]["ngReason"] = "過剰なRT企画ツイートがある"
         else:
             print(f"{reply['name']}:ok")
 
-    #日本語チェック
+    # 日本語チェック
     print("日本語チェック")
-    pattern = "[\u3041-\u309F]+" #ひらがな
-    for idx,reply in enumerate(replyList):
+    pattern = "[\u3041-\u309F]+"  # ひらがな
+    for idx, reply in enumerate(replyList):
         print(re.findall(pattern, reply["profile"]))
         if len(re.findall(pattern, reply["profile"])) == 0:
             print(f"{reply['name']}:ng")
@@ -154,7 +187,7 @@ def main():
         else:
             print(f"{reply['name']}:ok")
 
-    #リプライする
+    # リプライする
     for reply in replyList:
         if reply["ng"]:
             status = f'{reply["name"]}さん、企画に参加ありがとうございます！\n大変申し訳ありませんが、{reply["ngReason"]}のため、{reply["name"]}さんのツイートはRTできません。\n\n※このツイートはbotからの自動送信です'
@@ -165,14 +198,13 @@ def main():
         print(f"in_reply_to_status_id:{in_reply_to_status_id} exec")
         replyResponse = requests.post(
             f'https://api.twitter.com/1.1/statuses/update.json',
-            data={"status":status
-                ,"in_reply_to_status_id":in_reply_to_status_id
-                ,"auto_populate_reply_metadata":True},
+            data={"status": status, "in_reply_to_status_id": in_reply_to_status_id,
+                  "auto_populate_reply_metadata": True},
             auth=getOauth()
         ).json()
         print(replyResponse)
 
-        #リツイートする
+        # リツイートする
         if not reply["ng"]:
             for idx in range(3):
                 if idx >= len(reply["tweetIdList"]):
@@ -188,6 +220,7 @@ def main():
                 ).json()
                 print(response)
 
+
 def lambda_handler(event, context):
 
     try:
@@ -196,4 +229,3 @@ def lambda_handler(event, context):
     except:
         import traceback
         traceback.print_exc()
-
--- src/twitterLotteryBot/lambda_function.py
+++ src/twitterLotteryBot/lambda_function.py
@@ -8,6 +8,7 @@ import datetime
 import collections
 import re
 import random
+import boto3
 
 OAUTH1 = os.environ['OAUTH1']
 OAUTH2 = os.environ['OAUTH2']
@@ -15,27 +16,66 @@ OAUTH3 = os.environ['OAUTH3']
 OAUTH4 = os.environ['OAUTH4']
 SCREEN_NAME = os.environ['SCREEN_NAME']
 
+
 def getOauth():
     return OAuth1(OAUTH1, OAUTH2, OAUTH3, OAUTH4)
 
+
+def getParam(key):
+    dynamoDB = boto3.resource("dynamodb")
+    paramTable = dynamoDB.Table("twitterLotPram")
+    item = paramTable.get_item(Key={"key": key})
+    if "Item" in item:
+        return item["Item"]
+    else:
+        return []
+
+
+def putParam(key, data):
+    dynamoDB = boto3.resource("dynamodb")
+    paramTable = dynamoDB.Table("twitterLotPram")
+    paramTable.put_item(Item={"key": key, "data": data})
+
+
+def getItem(key):
+    dynamoDB = boto3.resource("dynamodb")
+    table = dynamoDB.Table("twitterLotterBot")
+    item = table.get_item(Key=key)
+    if "Item" in item:
+        return item["Item"]
+    else:
+        return None
+
+
+def putItem(data):
+    dynamoDB = boto3.resource("dynamodb")
+    table = dynamoDB.Table("twitterLotterBot")
+    table.put_item(Item=data)
+
+
 def main():
 
-    #botのツイートを取得
+    dynamoDB = boto3.resource("dynamodb")
+    table = dynamoDB.Table("twitterLotterBot")
+
+    # botのツイートを取得
     doneList = requests.get(
-        f'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={SCREEN_NAME}&count=100',
+        f'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={SCREEN_NAME}&count=10',
         auth=getOauth()
     ).json()
     if "errors" in doneList:
         print(doneList)
         return
-    doneList = list(map(lambda x:x["in_reply_to_status_id"],doneList))
+    doneList = getParam("doneList") + \
+        list(map(lambda x: x["in_reply_to_status_id"], doneList))
+    doneList = list(set(doneList))
+    putParam("doneList", doneList)
     print(doneList)
 
-
-    #指定タグのツイートを取得
+    # 指定タグのツイートを取得
     search = '%23抽選bot'
     response = requests.get(
-        f'https://api.twitter.com/1.1/search/tweets.json?q={search}&count=10&lang=ja&result_type=mixed',
+        f'https://api.twitter.com/1.1/search/tweets.json?q={search}&count=50&lang=ja&result_type=mixed&tweet_mode=extended',
         auth=getOauth()
     ).json()
 
@@ -45,72 +85,134 @@ def main():
 
     statusList = response["statuses"]
 
+    tweetList = getParam("tweetList") + \
+        list(map(lambda x: x["statuses"]['id'], response))
+    tweetList = list(set(tweetList))
+    putParam("tweetList", tweetList)
+
     for status in statusList:
+
         if "retweeted_status" in status:
             print("RT skip")
             continue
         if status["in_reply_to_status_id"]:
             print("rep skip")
             continue
-            
-        print(f"text:{status['text']}")
+
+        data = {"statusId": status['id']}
+        data = getItem(data)
+
         tweet_id = status['id']
-        print(f"tweet_id:{tweet_id}")
-        print(f"user_id:{status['user']['id']}")
-        print(f"user_screen_name:{status['user']['screen_name']}")
-        print(status)
+        main_text = status['full_text']
+
+        if "…" in main_text:
+            tmp = requests.get(
+                f'https://api.twitter.com/1.1/statuses/show.json?id={tweet_id}&tweet_mode=extended',
+                auth=getOauth()
+            ).json()
+            main_text = tmp['full_text']
+
+        data["text"] = main_text
+        data["user_id"] = status['user']['id']
+        data["user_screen_name"] = status['user']['screen_name']
+
+        lot_pro = re.findall(r'(\[.+?\]\(\d+)', main_text)
+        lot_pro = list(map(lambda x: {"name": re.findall(
+            r'\[(.+)\]', x)[0], "pro": int(re.findall(r'(\d+)', x)[0])}, lot_pro))
+        data["lot_pro"] = lot_pro
+
+        lot_message = ""
+        for p in lot_pro:
+            lot_message += f'{p["name"]}:{p["pro"]}%,'
+        data["lot_message"] = lot_message
+
+        if tweet_id not in doneList:
+            print(f"{tweet_id} first reply exec")
+
+            # リプライする
+            if len(lot_pro) == 0:
+                message = f'#抽選bot のご利用ありがとうございます！\n申し訳ありませんが、確立を読み取れませんでした。\n正しい使い方などは↓のリンクをご覧ください。\nhttps://encr.jp/blog/posts/20200306_morning/\n\n※このツイートはbotからの自動送信です'
+            else:
+                message = f'#抽選bot のご利用ありがとうございます！\n{lot_message[:-1]}で抽選いたします。\nhttps://encr.jp/blog/posts/20200306_morning/\n\n※このツイートはbotからの自動送信です'
+            data["main_reply_message"] = message
+            in_reply_to_status_id = tweet_id
+            replyResponse = requests.post(
+                f'https://api.twitter.com/1.1/statuses/update.json',
+                data={"status": message, "in_reply_to_status_id": in_reply_to_status_id,
+                      "auto_populate_reply_metadata": True},
+                auth=getOauth()
+            ).json()
+
+            if "errors" in replyResponse:
+                print(replyResponse)
+                return
+
+        if len(lot_pro) == 0:
+            putItem(data)
+            continue
+
         sleepTime = random.random() * 20
-        
-        #指定タグのツイート者宛のリプライを取得
+        time.sleep(sleepTime)
+
+        # 指定タグのツイート者宛のリプライを取得
         search = f"%40{status['user']['screen_name']}"
-        rep_response = requests.get(
+        repResponse = requests.get(
             f'https://api.twitter.com/1.1/search/tweets.json?q={search}&count=10&lang=ja&result_type=mixed',
             auth=getOauth()
         ).json()
-        
-        if "errors" in rep_response:
-            print(rep_response)
+
+        if "errors" in repResponse:
+            print(repResponse)
             continue
-        
-        rep_statusList = rep_response["statuses"]
-        
-        for rep_status in rep_statusList:
+
+        repStatusList = repResponse["statuses"]
+
+        for rep_status in repStatusList:
             if "retweeted_status" in status:
                 print("RT skip")
                 continue
-            
+
             if rep_status['in_reply_to_status_id'] != tweet_id:
                 continue
 
-            if rep_status['id'] in doneList:
+            if rep_status['id'] in doneList or rep_status['user']['screen_name'] == SCREEN_NAME:
                 print(f"{rep_status['user']['name']} reply done")
                 continue
 
             print(f"text:{rep_status['text']}")
             print(f"tweet_id:{rep_status['id']}")
             print(f"user_id:{rep_status['user']['id']}")
+            print(f"user_screen_name:{rep_status['user']['screen_name']}")
             print(f"user_name:{rep_status['user']['name']}")
 
-            #リプライする
-            if random.random() <= 0.1:
-                lot = "大吉"
-            elif random.random() <= 0.6:
-                lot = "中吉"
+            # 抽選する
+            sump = 0
+            yourp = random.random() * 100
+            lot = None
+            for p in lot_pro:
+                sump += p["pro"]
+                if yourp <= sump:
+                    lot = p["name"]
+                    break
+
+            # リプライする
+            if lot is not None:
+                message = f'{rep_status["user"]["name"]}さん、リプありがとうございます！\n「{lot}」が当選しました。\n\n※このツイートはbotからの自動送信です #抽選bot'
             else:
-                lot = "小吉"
-            message = f'{rep_status["user"]["name"]}さん、リプありがとうございます！\n「{lot}」が当選しました。だだいまテストツイート中です。\n\n※このツイートはbotからの自動送信です'
+                message = f'{rep_status["user"]["name"]}さん、リプありがとうございます！\n残念ながら、今回は落選してしまいました…\n\n※このツイートはbotからの自動送信です #抽選bot'
+
             in_reply_to_status_id = rep_status['id']
             print(f"message:{message} exec")
             print(f"in_reply_to_status_id:{in_reply_to_status_id} exec")
             replyResponse = requests.post(
-                 f'https://api.twitter.com/1.1/statuses/update.json',
-                 data={"status":message
-                     ,"in_reply_to_status_id":in_reply_to_status_id
-                     ,"auto_populate_reply_metadata":True},
-             auth=getOauth()
+                f'https://api.twitter.com/1.1/statuses/update.json',
+                data={"status": message, "in_reply_to_status_id": in_reply_to_status_id,
+                      "auto_populate_reply_metadata": True},
+                auth=getOauth()
             ).json()
             print(replyResponse)
-        
+        putItem(data)
+
 
 def lambda_handler(event, context):
 
@@ -120,4 +222,3 @@ def lambda_handler(event, context):
     except:
         import traceback
         traceback.print_exc()
-
